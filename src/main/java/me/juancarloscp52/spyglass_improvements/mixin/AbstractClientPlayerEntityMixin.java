59cb2f5dbae2e6cf02ff087e0f12092a91a7f009
jc <juancarloscp52@gmail.com>
2022-01-30 19:45:01 +0100

    Change method for applying spyglass-zoom. (Fixes #1)

---
+++ src/main/java/me/juancarloscp52/spyglass_improvements/mixin/AbstractClientPlayerEntityMixin.java
@@ -0,0 +1,31 @@
+package me.juancarloscp52.spyglass_improvements.mixin;
+
+import com.mojang.authlib.GameProfile;
+import me.juancarloscp52.spyglass_improvements.client.SpyglassImprovementsClient;
+import net.minecraft.client.MinecraftClient;
+import net.minecraft.client.network.AbstractClientPlayerEntity;
+import net.minecraft.client.world.ClientWorld;
+import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.world.World;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+
+@Mixin(AbstractClientPlayerEntity.class)
+public abstract class AbstractClientPlayerEntityMixin extends PlayerEntity {
+
+
+    public AbstractClientPlayerEntityMixin(World world, BlockPos pos, float yaw, GameProfile profile) {
+        super(world, pos, yaw, profile);
+    }
+
+    @Inject(method = "getFovMultiplier", at = @At("RETURN"), cancellable = true)
+    public void fovMultiplier(CallbackInfoReturnable<Float> cir){
+        if(MinecraftClient.getInstance().options.getPerspective().isFirstPerson() && isUsingSpyglass())
+            cir.setReturnValue(SpyglassImprovementsClient.MULTIPLIER);
+    }
+
+}
