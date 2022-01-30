7731d70d1cf1e7ebb2ddb38d6e93b794314c24b5
jc <juancarloscp52@gmail.com>
2022-01-30 16:07:27 +0100

    Smooth zoom in/out

--- src/main/java/me/juancarloscp52/spyglass_improvements/mixin/GameRendererMixin.java
+++ src/main/java/me/juancarloscp52/spyglass_improvements/mixin/GameRendererMixin.java
@@ -4,7 +4,9 @@ import me.juancarloscp52.spyglass_improvements.client.SpyglassImprovementsClient
 import net.minecraft.client.MinecraftClient;
 import net.minecraft.client.network.ClientPlayerEntity;
 import net.minecraft.client.render.GameRenderer;
+import net.minecraft.util.math.MathHelper;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.gen.Accessor;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
@@ -13,15 +15,18 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 @Mixin(GameRenderer.class)
 public abstract class GameRendererMixin {
 
+    @Shadow private float fovMultiplier;
+    private float multiplier = SpyglassImprovementsClient.MULTIPLIER;
+
     @Inject(method = "updateFovMultiplier", at = @At("RETURN"))
     private void applyZoom(CallbackInfo ci){
         ClientPlayerEntity player = MinecraftClient.getInstance().player;
-        // Change fov depending on the spyglass zoom.
         if(player != null && player.isUsingSpyglass() && MinecraftClient.getInstance().options.getPerspective().isFirstPerson()){
-            setFovMultiplier(SpyglassImprovementsClient.MULTIPLIER);
+            multiplier = MathHelper.clampedLerp(
+                    multiplier,
+                    SpyglassImprovementsClient.MULTIPLIER,
+                    0.3f);
+            fovMultiplier = multiplier;
         }
     }
-
-    @Accessor("fovMultiplier")
-    public abstract void setFovMultiplier(float multiplier);
 }
