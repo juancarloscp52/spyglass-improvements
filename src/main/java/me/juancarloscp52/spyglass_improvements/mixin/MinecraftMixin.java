32ed978c3c8885c104e4e6779e05b2059d6e0187
Cassian Godsted <cassian@godsted.com>
2025-03-09 13:00:03 -0400

    Remove dangerous redirect

--- src/main/java/me/juancarloscp52/spyglass_improvements/mixin/MinecraftMixin.java
+++ src/main/java/me/juancarloscp52/spyglass_improvements/mixin/MinecraftMixin.java
@@ -1,16 +1,25 @@
 package me.juancarloscp52.spyglass_improvements.mixin;
 
+import com.llamalad7.mixinextras.injector.wrapoperation.Operation;
+import com.llamalad7.mixinextras.injector.wrapoperation.WrapOperation;
 import me.juancarloscp52.spyglass_improvements.client.SpyglassImprovementsClient;
 import net.minecraft.client.KeyMapping;
 import net.minecraft.client.Minecraft;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.Redirect;
 
 @Mixin(Minecraft.class)
 public class MinecraftMixin {
-    @Redirect(method = "handleKeybinds", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/KeyMapping;isDown()Z", ordinal = 2))
-    public boolean handleInput(KeyMapping instance){
-        return instance.isDown() || SpyglassImprovementsClient.useSpyglass.isDown();
+
+    @WrapOperation(
+            method = "handleKeybinds",
+            at = @At(value = "INVOKE", target = "Lnet/minecraft/client/KeyMapping;isDown()Z")
+    )
+    private boolean handleSpyglassInput(KeyMapping instance, Operation<Boolean> original) {
+        if (SpyglassImprovementsClient.useSpyglass.isDown()) {
+            return true;
+        } else {
+            return original.call(instance);
+        }
     }
 }
